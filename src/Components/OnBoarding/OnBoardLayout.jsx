193361eaa808d3dd66bb65c8ac219621d54f8148
masudpro121 <masud.pro.121@gmail.com>
2023-10-31 11:25:46 +0600

    left layout style fix

--- src/Components/OnBoarding/OnBoardLayout.jsx
+++ src/Components/OnBoarding/OnBoardLayout.jsx
@@ -10,61 +10,62 @@ import { MyContext } from "@/pages/_app";
 import Header from "../Header/Header";
 
 const OnBoardLayout = (props) => {
-  const {onBoardingStep, setOnBoardingStep} = useContext(MyContext)
-  const [onBoard, setOnBoard] = useState({})
+  const { onBoardingStep, setOnBoardingStep } = useContext(MyContext);
+  const [onBoard, setOnBoard] = useState({});
   useEffect(() => {
     const ob = JSON.parse(localStorage.getItem("onBoard")) || {};
-    setOnBoard(ob)
+    setOnBoard(ob);
   }, []);
 
   const leftMenu = [
     {
       title: "I am",
       description: "Tell us a bit about yourself.",
-      step:1,
-      target: 'who'
+      step: 1,
+      target: "who",
     },
     {
       title: "Select Style",
       description: "Choose your preferred design style.",
-      step:2,
-      target: 'style'
+      step: 2,
+      target: "style",
     },
     {
       title: "Select room type",
       description: "Pick or type the room type you're envisioning",
-      step:3,
-      target: 'type'
+      step: 3,
+      target: "type",
     },
     {
       title: "Select place",
+      // description: "Select best describes place",
       description: "Click on a label that best describes your location.",
-      step:4,
-      target: 'place'
+      step: 4,
+      target: "place",
     },
     {
       title: "Select Room Size",
       description: "Upload your room image",
-      step:5,
-      target: 'size'
+      step: 5,
+      target: "size",
     },
     {
       title: "Enter Your Budget",
       description: "Please provide your name and email",
-      step:6,
-      target: 'budget'
+      step: 6,
+      target: "budget",
     },
     {
       title: "Choose ",
       description: "Please provide your name and email",
-      step:7,
-      target: 'haveSpace'
+      step: 7,
+      target: "haveSpace",
     },
     {
       title: "Upload File",
       description: "Please provide your name and email",
-      step:8,
-      target: ''
+      step: 8,
+      target: "",
     },
   ];
   const handleStep = (st) => {
@@ -88,28 +89,48 @@ const OnBoardLayout = (props) => {
 
       <div className="md:p-6">
         <div className="custom-container md:py-3 flex flex-col md:flex-row gap-[10px]  bg-white rounded">
-          <div className="hidden md:block left py-6 pl-4 pr-[10px] border-r border-[#D0D5DD]">
-            <div className="flex flex-col space-y-[10px]">
+          <div className="hidden md:block left py-6 pl-3 pr-[10px] w-[380px] border-r border-[#D0D5DD]">
+            <div className="flex  flex-col space-y-[10px]">
               {leftMenu.map((item, index) => {
-                 if(onBoard[item.target] || onBoardingStep==item.step){
+                if (onBoard[item.target] || onBoardingStep == item.step) {
                   return (
                     <div
-                      onClick={()=>handleStep(item.step)}
+                      onClick={() => handleStep(item.step)}
                       key={index}
-                      className={`py-[12px] pl-[10px] w-[300px] pr-[8px] bg-[#F7F8F9] rounded-tr-lg rounded-br-lg flex space-x-[12px]  ${item.step==onBoardingStep && "border-l-2 border-[#9D5C0D]"} cursor-pointer`}
+                      className={`p-2  rounded-tr-lg rounded-br-lg flex gap-3 items-center  ${
+                        item.step == onBoardingStep &&
+                        "border-l-2  border-[#9D5C0D] bg-[#F7F8F9]"
+                      } cursor-pointer`}
                     >
-                      <Image className="w-8" src={item.step==onBoardingStep?menuIconBrown:menuIconGreen} alt="" />
-                      <div className="">
+                      <div className="w-10  flex items-center justify-start">
+                        <Image
+                          className=""
+                          src={
+                            item.step == onBoardingStep
+                              ? menuIconBrown
+                              : menuIconGreen
+                          }
+                          alt=""
+                        />
+                      </div>
+                      <div className="w-full">
                         <h2 className="text-[16px] text-[#344054] font-[Gilroy-SemiBold]">
                           {item.title}
                         </h2>
-                        <p className={`text-[14px]  ${(onBoard[item.target] && !(onBoardingStep==item.step)) ?'text-[#98AE1A]':' text-[#9D5C0D]'}`}>
+                        <p
+                          className={`text-[14px]  ${
+                            onBoard[item.target] &&
+                            !(onBoardingStep == item.step)
+                              ? "text-[#98AE1A]"
+                              : " text-[#9D5C0D]"
+                          }`}
+                        >
                           {item.description}
                         </p>
                       </div>
                     </div>
                   );
-                 }
+                }
               })}
             </div>
           </div>
